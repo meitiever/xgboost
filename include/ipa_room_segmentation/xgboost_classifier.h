e4fa07539d3c06b35e6f382a121c3b7410c70cd8
meitiever <meitiever@gmail.com>
2021-07-09 17:15:44 +0800

    update xgboost predictor.

--- include/ipa_room_segmentation/xgboost_classifier.h
+++ include/ipa_room_segmentation/xgboost_classifier.h
@@ -1,217 +1,90 @@
 #pragma once
 
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <list>
-#include <vector>
-#include <math.h>
-#include <fstream>
 #include <string>
-
-#include <ctime>
-#include <stdlib.h>
-#include <rabit/rabit.h>
-
-#include <xgboost/learner.h>
-#include <xgboost/data.h>
-#include <xgboost/json.h>
-#include <xgboost/logging.h>
-#include <xgboost/parameter.h>
-#include "../src/common/io.h"
+#include <xgboost/c_api.h>
+#include <math.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <Eigen/Eigen>
+#include <iostream>
 
 #include <ipa_room_segmentation/features.h>
 #include <ipa_room_segmentation/raycasting.h>
 
-using namespace xgboost;
-
-enum CLITask {
-  kTrain = 0,
-  kDumpModel = 1,
-  kPredict = 2
-};
-
-struct CLIParam : public XGBoostParameter<CLIParam> {
-  /*! \brief the task name */
-  int task;
-  /*! \brief whether evaluate training statistics */
-  bool eval_train;
-  /*! \brief number of boosting iterations */
-  int num_round;
-  /*! \brief the period to save the model, 0 means only save the final round model */
-  int save_period;
-  /*! \brief the path of training set */
-  std::string train_path;
-  /*! \brief path of test dataset */
-  std::string test_path;
-  /*! \brief the path of test model file, or file to restart training */
-  std::string model_in;
-  /*! \brief the path of final model file, to be saved */
-  std::string model_out;
-  /*! \brief the path of directory containing the saved models */
-  std::string model_dir;
-  /*! \brief name of predict file */
-  std::string name_pred;
-  /*! \brief data split mode */
-  int dsplit;
-  /*!\brief limit number of trees in prediction */
-  int ntree_limit;
-  int iteration_begin;
-  int iteration_end;
-  /*!\brief whether to directly output margin value */
-  bool pred_margin;
-  /*! \brief whether dump statistics along with model */
-  int dump_stats;
-  /*! \brief what format to dump the model in */
-  std::string dump_format;
-  /*! \brief name of feature map */
-  std::string name_fmap;
-  /*! \brief name of dump file */
-  std::string name_dump;
-  /*! \brief the paths of validation data sets */
-  std::vector<std::string> eval_data_paths;
-  /*! \brief the names of the evaluation data used in output log */
-  std::vector<std::string> eval_data_names;
-  /*! \brief all the configurations */
-  std::vector<std::pair<std::string, std::string> > cfg;
-
-  static constexpr char const* const kNull = "NULL";
-
-  // declare parameters
-  DMLC_DECLARE_PARAMETER(CLIParam) {
-    // NOTE: declare everything except eval_data_paths.
-    DMLC_DECLARE_FIELD(task).set_default(kTrain)
-      .add_enum("train", kTrain)
-      .add_enum("dump", kDumpModel)
-      .add_enum("pred", kPredict)
-      .describe("Task to be performed by the CLI program.");
-    DMLC_DECLARE_FIELD(eval_train).set_default(false)
-      .describe("Whether evaluate on training data during training.");
-    DMLC_DECLARE_FIELD(num_round).set_default(10).set_lower_bound(1)
-      .describe("Number of boosting iterations");
-    DMLC_DECLARE_FIELD(save_period).set_default(0).set_lower_bound(0)
-      .describe("The period to save the model, 0 means only save final model.");
-    DMLC_DECLARE_FIELD(train_path).set_default("NULL")
-      .describe("Training data path.");
-    DMLC_DECLARE_FIELD(test_path).set_default("NULL")
-      .describe("Test data path.");
-    DMLC_DECLARE_FIELD(model_in).set_default("NULL")
-      .describe("Input model path, if any.");
-    DMLC_DECLARE_FIELD(model_out).set_default("NULL")
-      .describe("Output model path, if any.");
-    DMLC_DECLARE_FIELD(model_dir).set_default("./")
-      .describe("Output directory of period checkpoint.");
-    DMLC_DECLARE_FIELD(name_pred).set_default("pred.txt")
-      .describe("Name of the prediction file.");
-    DMLC_DECLARE_FIELD(dsplit).set_default(0)
-      .add_enum("auto", 0)
-      .add_enum("col", 1)
-      .add_enum("row", 2)
-      .describe("Data split mode.");
-    DMLC_DECLARE_FIELD(ntree_limit).set_default(0).set_lower_bound(0)
-      .describe("(Deprecated) Use iteration_begin/iteration_end instead.");
-    DMLC_DECLARE_FIELD(iteration_begin).set_default(0).set_lower_bound(0)
-      .describe("Begining of boosted tree iteration used for prediction.");
-    DMLC_DECLARE_FIELD(iteration_end).set_default(0).set_lower_bound(0)
-      .describe("End of boosted tree iteration used for prediction.  0 means all the trees.");
-    DMLC_DECLARE_FIELD(pred_margin).set_default(false)
-      .describe("Whether to predict margin value instead of probability.");
-    DMLC_DECLARE_FIELD(dump_stats).set_default(false)
-      .describe("Whether dump the model statistics.");
-    DMLC_DECLARE_FIELD(dump_format).set_default("text")
-      .describe("What format to dump the model in.");
-    DMLC_DECLARE_FIELD(name_fmap).set_default("NULL")
-      .describe("Name of the feature map file.");
-    DMLC_DECLARE_FIELD(name_dump).set_default("dump.txt")
-      .describe("Name of the output dump text file.");
-    // alias
-    DMLC_DECLARE_ALIAS(train_path, data);
-    DMLC_DECLARE_ALIAS(test_path, test:data);
-    DMLC_DECLARE_ALIAS(name_fmap, fmap);
-  }
-  // customized configure function of CLIParam
-  inline void Configure(const std::vector<std::pair<std::string, std::string> >& _cfg) {
-    // Don't copy the configuration to enable parameter validation.
-    auto unknown_cfg = this->UpdateAllowUnknown(_cfg);
-    this->cfg.emplace_back("validate_parameters", "True");
-    for (const auto& kv : unknown_cfg) {
-      if (!strncmp("eval[", kv.first.c_str(), 5)) {
-        char evname[256];
-        CHECK_EQ(sscanf(kv.first.c_str(), "eval[%[^]]", evname), 1)
-          << "must specify evaluation name for display";
-        eval_data_names.emplace_back(evname);
-        eval_data_paths.push_back(kv.second);
-      }
-      else {
-        this->cfg.emplace_back(kv);
-      }
-    }
-    // constraint.
-    if (name_pred == "stdout") {
-      save_period = 0;
-    }
-    if (dsplit == 0 && rabit::IsDistributed()) {
-      dsplit = 2;
-    }
-  }
-};
-
-class XgboostClassifier {
-protected:
-  std::unique_ptr<Learner> learner_;
-  bool trained_; // variable that shows if the classifiers has already been trained
-  CLIParam param_;
-  std::vector<double> angles_for_simulation_; // angle-vector used to calculate the features for this algorithm
-  LaserScannerRaycasting raycasting_;
-
+class XgboostClassifier
+{
 public:
-
-  XgboostClassifier(std::string& model_path);
-
-  int resetLearner(std::vector<std::shared_ptr<DMatrix>> const& matrices) {
-    learner_.reset(Learner::Create(matrices));
-    int version = rabit::LoadCheckPoint(learner_.get());
-    if (version == 0) {
-      if (param_.model_in != CLIParam::kNull) {
-        this->loadModel(param_.model_in, learner_.get());
-        learner_->SetParams(param_.cfg);
-      }
-      else {
-        learner_->SetParams(param_.cfg);
-      }
-    }
-    learner_->Configure();
-    return version;
+  typedef Eigen::Matrix<float, -1, -1, Eigen::RowMajor> Matrix;
+  template<typename M>
+  static void vector2Matrix(M& m, const typename M::Scalar* vec, Eigen::Index const rows, Eigen::Index const cols)
+  {
+    m = Eigen::Map<const Eigen::Matrix<typename M::Scalar, M::RowsAtCompileTime, M::ColsAtCompileTime, Eigen::RowMajor>>(vec, rows, cols);
   }
 
-  void saveModel(std::string const& path, Learner* learner) const {
-    learner->Configure();
-    std::unique_ptr<dmlc::Stream> fo(dmlc::Stream::Create(path.c_str(), "w"));
-    if (common::FileExtension(path) == "json") {
-      Json out{ Object() };
-      learner->SaveModel(&out);
-      std::string str;
-      Json::Dump(out, &str);
-      fo->Write(str.c_str(), str.size());
+  XgboostClassifier(std::string const& model_path, uint64_t nlabels = 1) :
+    _modelPath(model_path),
+    _nlabels(nlabels)
+  {
+    for (double angle = 0; angle < 360; angle++) {
+      angles_for_simulation_.push_back(angle);
     }
-    else {
-      learner->SaveModel(fo.get());
-    }
-  }
 
-  void loadModel(std::string const& path, Learner* learner) const {
-    if (common::FileExtension(path) == "json") {
-      auto str = common::LoadSequentialFile(path);
-      CHECK_GT(str.size(), 2);
-      CHECK_EQ(str[0], '{');
-      Json in{ Json::Load({str.c_str(), str.size()}) };
-      learner->LoadModel(in);
+    if (XGBoosterCreate(NULL, 0, &_booster) == 0 && XGBoosterLoadModel(_booster, _modelPath.c_str()) == 0) {
+      //LOG HERE
+      trained_ = true;
     }
     else {
-      std::unique_ptr<dmlc::Stream> fi(dmlc::Stream::Create(path.c_str(), "r"));
-      learner->LoadModel(fi.get());
+      //LOG HERE
+      _booster = NULL;
     }
   }
 
   //labeling-algorithm after the training
-  bool segmentMap(const cv::Mat& map_to_be_labeled, cv::Mat& segmented_map, double map_resolution_from_subscription);
+  bool segmentMap(const cv::Mat& map_to_be_labeled, cv::Mat& segmented_map, double map_resolution_from_subscription,
+    double room_area_factor_lower_limit, double room_area_factor_upper_limit);
+
+  virtual ~XgboostClassifier() {
+    XGBoosterFree(_booster);
+  }
+
+private:
+  int predict(Matrix const& features, Matrix& result)
+  {
+    DMatrixHandle X;
+    const float* data = features.data();
+    auto const nrow = features.rows();
+    auto const ncol = features.cols();
+
+    XGDMatrixCreateFromMat(data, nrow, ncol, NAN, &X);
+
+    const float* out;
+    uint64_t l;
+    auto ret = XGBoosterPredict(_booster, X, 0, 0, 0, &l, &out);
+    if (ret < 0) {
+      // LOG HERE
+      return -1;
+    }
+
+    XGDMatrixFree(X);
+
+    if (l != nrow * _nlabels) {
+      //LOG HERE
+      return -1;
+    }
+
+    vector2Matrix(result, out, nrow, _nlabels);
+    return 0;
+  }
+
+private:
+  bool trained_; // variable that shows if the classifiers has already been trained
+  std::string const _modelPath;
+  BoosterHandle _booster;
+  uint64_t const _nlabels;
+  std::string model_path;
+  std::vector<double> angles_for_simulation_; // angle-vector used to calculate the features for this algorithm
+  LaserScannerRaycasting raycasting_;
 };
--- src/ipa_room_segmentation/xgboost_classifier.cpp
+++ src/ipa_room_segmentation/xgboost_classifier.cpp
@@ -8,39 +8,17 @@
 #include <ipa_room_segmentation/xgboost_classifier.h>
 #include <ipa_room_segmentation/wavefront_region_growing.h>
 
-#include "../src/common/config.h"
+bool XgboostClassifier::segmentMap(const cv::Mat& map_to_be_labeled, cv::Mat& segmented_map, double map_resolution_from_subscription,
+  double room_area_factor_lower_limit, double room_area_factor_upper_limit) {
+  std::cout << "Start prediction..." << std::endl;
 
-XgboostClassifier::XgboostClassifier(std::string& config_path) {
-  for (double angle = 0; angle < 360; angle++) {
-    angles_for_simulation_.push_back(angle);
-  }
+  cv::Mat original_map_to_be_labeled = map_to_be_labeled.clone();
 
-  trained_ = false;
-  //std::string model = model_path + "semantic_room_xgboost_r100.model";
-
-  common::ConfigParser cp(config_path);
-  auto cfg = cp.Parse();
-
-  param_.Configure(cfg);
-}
-
-bool XgboostClassifier::segmentMap(const cv::Mat& map_to_be_labeled, cv::Mat& segmented_map, double map_resolution_from_subscription) {
-  if (!trained_) {
-    // load model
-    resetLearner({});
-  }
-
-  std::cout << "Start prediction...";
-  HostDeviceVector<bst_float> preds;
-  //if (param_.ntree_limit != 0) {
-  //  param_.iteration_end = getIterationFromTreeLimit(param_.ntree_limit, learner_.get());
-  //  LOG(WARNING) << "`ntree_limit` is deprecated, use `iteration_begin` and "
-  //    "`iteration_end` instead.";
-  //}
   std::vector<double> temporary_beams;
   std::vector<float> temporary_features;
   LaserScannerFeatures lsf;
 
+  assert(_booster != NULL && trained_);
   for (int y = 0; y < map_to_be_labeled.rows; y++) {
     for (int x = 0; x < map_to_be_labeled.cols; x++) {
       if (map_to_be_labeled.at<unsigned char>(y, x) != 0) {
@@ -50,26 +28,195 @@ bool XgboostClassifier::segmentMap(const cv::Mat& map_to_be_labeled, cv::Mat& se
         lsf.get_features(temporary_beams, angles_for_simulation_, cv::Point(x, y), features);
         temporary_features.resize(features.cols);
 
-        for (int i = 0; i < features.cols; ++i)
-          temporary_features[i] = features.at<float>(0, i);
+        Matrix d_features(1, 23);
+        d_features << features.at<float>(0, 0), features.at<float>(0, 1), features.at<float>(0, 2), features.at<float>(0, 3), features.at<float>(0, 4)
+          , features.at<float>(0, 5), features.at<float>(0, 6), features.at<float>(0, 7), features.at<float>(0, 8), features.at<float>(0, 9)
+          , features.at<float>(0, 10), features.at<float>(0, 11), features.at<float>(0, 12), features.at<float>(0, 13), features.at<float>(0, 14)
+          , features.at<float>(0, 15), features.at<float>(0, 16), features.at<float>(0, 17), features.at<float>(0, 18), features.at<float>(0, 19)
+          , features.at<float>(0, 20), features.at<float>(0, 21), features.at<float>(0, 22);
 
-        // convert temporary_features to DMatrix
-        std::shared_ptr<DMatrix> dtest(DMatrix::Load(param_.test_path, ConsoleLogger::GlobalVerbosity() > ConsoleLogger::DefaultVerbosity(), param_.dsplit == 2));
-        learner_->Predict(dtest, param_.pred_margin, &preds, param_.iteration_begin, param_.iteration_end);
-        LOG(CONSOLE) << "Writing prediction to " << param_.name_pred;
-        temporary_features.clear();
+        Matrix y;
+        int ret = predict(d_features, y);
+        if (ret != 0) {
+          std::cout << "predict error" << std::endl;
+        }
+
+        double probability_for_room = y[0];
+        double probability_for_hallway = 1.0 - probability_for_room;
+
+        if (probability_for_room > probability_for_hallway) {
+          original_map_to_be_labeled.at<unsigned char>(y, x) = 150; //label it as room
+        }
+        else {
+          original_map_to_be_labeled.at<unsigned char>(y, x) = 100; //label it as hallway
+        }
       }
     }
   }
 
-  std::unique_ptr<dmlc::Stream> fo(dmlc::Stream::Create(param_.name_pred.c_str(), "w"));
-  dmlc::ostream os(fo.get());
-  for (bst_float p : preds.ConstHostVector()) {
-    os << std::setprecision(std::numeric_limits<bst_float>::max_digits10) << p << '\n';
+  std::cout << "labeled all white pixels: " << std::endl;
+  //******************** III. Apply a median filter over the image to smooth the results.***************************
+  cv::Mat temporary_map = original_map_to_be_labeled.clone();
+  cv::medianBlur(temporary_map, temporary_map, 3);
+  std::cout << "blurred image" << std::endl;
+
+  //make regions black, that have been black before
+  for (int x = 0; x < original_map_to_be_labeled.rows; x++) {
+    for (int y = 0; y < original_map_to_be_labeled.cols; y++) {
+      if (original_map_to_be_labeled.at<unsigned char>(x, y) == 0) {
+        temporary_map.at<unsigned char>(x, y) = 0;
+      }
+    }
   }
-  // force flush before fo destruct.
-  os.set_stream(nullptr);
-  trained_ = true;
+
+  cv::Mat blured_image_for_thresholding = temporary_map.clone();
+
+  //*********** IV. Fill the large enough rooms with a random color and split the hallways into smaller regions*********
+  std::vector<std::vector<cv::Point> > contours, temporary_contours, saved_room_contours, saved_hallway_contours;
+  //hierarchy saves if the contours are hole-contours:
+  //hierarchy[{0,1,2,3}]={next contour (same level), previous contour (same level), child contour, parent contour}
+  //child-contour = 1 if it has one, = -1 if not, same for parent_contour
+  std::vector < cv::Vec4i > hierarchy;
+
+  std::vector < cv::Scalar > already_used_colors; //saving-vector for the already used coloures
+
+  //find the contours, which are labeled as a room
+  cv::threshold(temporary_map, temporary_map, 120, 255, cv::THRESH_BINARY); //find rooms (value = 150)
+  cv::findContours(temporary_map, contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_NONE);
+  cv::drawContours(blured_image_for_thresholding, contours, -1, cv::Scalar(0), cv::FILLED); //make the found regions at the original map black, because they have been looked at already
+
+  //only take rooms that are large enough and that are not a hole-contour
+  for (int c = 0; c < contours.size(); c++) {
+    if (map_resolution_from_subscription * map_resolution_from_subscription * cv::contourArea(contours[c]) > room_area_factor_lower_limit
+      && hierarchy[c][3] != 1) {
+      saved_room_contours.push_back(contours[c]);
+    }
+  }
+
+  //find the contours, which are labeled as a hallway
+  map_to_be_labeled.convertTo(segmented_map, CV_32SC1, 256, 0);       // rescale to 32 int, 255 --> 255*256 = 65280
+  temporary_map = blured_image_for_thresholding.clone();
+
+  cv::threshold(temporary_map, temporary_map, 90, 255, cv::THRESH_BINARY); //find hallways (value = 100)
+  cv::findContours(temporary_map, contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_NONE);
+
+  //if the hallway-contours are too big split them into smaller regions, also don't take too small regions
+  for (int contour_counter = 0; contour_counter < contours.size(); contour_counter++) {
+    if (map_resolution_from_subscription * map_resolution_from_subscription * cv::contourArea(contours[contour_counter]) > room_area_factor_upper_limit) {
+      //Generate a black map to draw the hallway-contour in. Then use this map to ckeck if the generated random Points
+      // are inside the contour.
+      cv::Mat contour_Map = cv::Mat::zeros(temporary_map.rows, temporary_map.cols, CV_8UC1);
+      cv::drawContours(contour_Map, contours, contour_counter, cv::Scalar(255), cv::FILLED);
+      cv::erode(contour_Map, contour_Map, cv::Mat(), cv::Point(-1, -1), 10);
+      //center-counter so enough centers could be found
+      int center_counter = 0;
+      //saving-vector for watershed centers
+      std::vector < cv::Point > temporary_watershed_centers;
+
+      //find enough random watershed centers that are inside the hallway-contour
+      do {
+        int random_x = rand() % temporary_map.rows;
+        int random_y = rand() % temporary_map.cols;
+
+        if (contour_Map.at<unsigned char>(random_y, random_x) == 255) {
+          temporary_watershed_centers.push_back(cv::Point(random_x, random_y));
+          center_counter++;
+        }
+      } while (center_counter <= (map_resolution_from_subscription * map_resolution_from_subscription * cv::contourArea(contours[contour_counter])) / 8);
+
+      cv::Mat temporary_Map_to_wavefront;
+      contour_Map.convertTo(temporary_Map_to_wavefront, CV_32SC1, 256, 0);
+
+      //draw the centers as white circles into a black map and give the center-map and the contour-map to the opencv watershed-algorithm
+      for (int current_center = 0; current_center < temporary_watershed_centers.size(); current_center++) {
+        bool coloured = false;
+
+        do {
+          cv::Scalar fill_colour(rand() % 52224 + 13056);
+
+          if (!contains(already_used_colors, fill_colour)) {
+            cv::circle(temporary_Map_to_wavefront, temporary_watershed_centers[current_center], 2, fill_colour, cv::FILLED);
+            already_used_colors.push_back(fill_colour);
+            coloured = true;
+          }
+        } while (!coloured);
+      }
+
+      //make sure all previously black Pixels are still black
+      for (int x = 0; x < map_to_be_labeled.rows; x++) {
+        for (int y = 0; y < map_to_be_labeled.cols; y++) {
+          if (map_to_be_labeled.at<unsigned char>(x, y) == 0) {
+            temporary_Map_to_wavefront.at<unsigned char>(x, y) = 0;
+          }
+        }
+      }
+
+      wavefrontRegionGrowing(temporary_Map_to_wavefront);
+
+      //draw the seperated contour into the map, which should be labeled
+      for (int row = 0; row < segmented_map.rows; row++) {
+        for (int col = 0; col < segmented_map.cols; col++) {
+          if (temporary_Map_to_wavefront.at<int>(row, col) != 0) {
+            segmented_map.at<int>(row, col) = temporary_Map_to_wavefront.at<int>(row, col);
+          }
+        }
+      }
+    }
+    else
+      if (map_resolution_from_subscription * map_resolution_from_subscription * cv::contourArea(contours[contour_counter])
+        > room_area_factor_lower_limit) {
+        saved_hallway_contours.push_back(contours[contour_counter]);
+      }
+  }
+
+  std::cout << "finished too big hallway contours" << std::endl;
+
+  //draw every room and lasting hallway contour with a random colour into the map
+  for (int room = 0; room < saved_room_contours.size(); room++) {
+    bool coloured = false;
+
+    do {
+      cv::Scalar fill_colour(rand() % 52224 + 13056);
+
+      if (!contains(already_used_colors, fill_colour)) {
+        cv::drawContours(segmented_map, saved_room_contours, room, fill_colour, cv::FILLED);
+        already_used_colors.push_back(fill_colour);
+        coloured = true;
+      }
+    } while (!coloured);
+  }
+
+  std::cout << "finished room contours" << std::endl;
+
+  for (int hallway = 0; hallway < saved_hallway_contours.size(); hallway++) {
+    bool coloured = false;
+    int loop_counter = 0; //loop-counter to exit the loop if it gets a infite loop
+
+    do {
+      loop_counter++;
+      cv::Scalar fill_colour(rand() % 52224 + 13056);
+
+      if (!contains(already_used_colors, fill_colour) || loop_counter > 250) {
+        cv::drawContours(segmented_map, saved_hallway_contours, hallway, fill_colour, cv::FILLED);
+        already_used_colors.push_back(fill_colour);
+        coloured = true;
+      }
+    } while (!coloured);
+  }
+
+  std::cout << "finished small hallway contours" << std::endl;
+  //spread the coloured regions to regions, which were too small and aren't drawn into the map
+  wavefrontRegionGrowing(segmented_map);
+
+  //make sure previously black pixels are still black
+  for (int v = 0; v < map_to_be_labeled.rows; ++v) {
+    for (int u = 0; u < map_to_be_labeled.cols; ++u) {
+      if (map_to_be_labeled.at<unsigned char>(v, u) == 0) {
+        segmented_map.at<int>(v, u) = 0;
+      }
+    }
+  }
+
   std::cout << "Finished Labeling the map." << std::endl;
-  return true;
+  return trained_;
 }
